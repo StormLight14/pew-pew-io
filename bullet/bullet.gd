36be97a92ff19785778bfe3d7e45a5a4ecc30347
Storm <stormpower44@protonmail.com>
2023-12-30 16:08:12 -0700

    bullets die at obstacles

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -10,3 +10,7 @@ var player_id = 0
 func _physics_process(delta):
 	velocity = (bullet_direction).normalized()  * speed
 	global_position += velocity * delta
+
+func _on_body_entered(body):
+	if body.is_obstacle:
+		queue_free()
