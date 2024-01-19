fc5c436662e0229c7c6367178c1f985b0813cb74
Storm <stormpower44@protonmail.com>
2024-01-18 19:59:01 -0700

    add start of inv UI

--- level/level.gd
+++ level/level.gd
@@ -11,9 +11,7 @@ var ct_round_wins = 0
 func _ready():
 	visible = false
 	$GameUI.visible = false
-	
-	#if multiplayer.is_server():
-		#$CanvasModulate.color = Color(1, 1, 1, 1)
+
 
 func _process(_delta):
 	if start_timer.is_stopped() == false:
